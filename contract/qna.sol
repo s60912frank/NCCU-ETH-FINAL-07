6b6e78e9e08786f195716c83e86aa40d518a0d70
Gmin <s60912frank@gmail.com>
2019-01-02 10:50:14 +0800

    合約: 現在可以斗內問題/斗內留言可以分給提問/階層式留言ok/只有最高階的留言視為回答

--- contract/qna.sol
+++ contract/qna.sol
@@ -5,18 +5,22 @@ contract QnA {
     mapping (address => User) public members;
     Question[] public questions;
     Comment[] public comments;
-
+    uint32 donate_ratio;
+    
+    // events
     event registered(address user, string nickname);
-    event questionAsked(address user, uint qId);
-    event commentAdded(address user, uint qId, uint cId);
-    event questionAnswered(uint qId, uint cId, uint reward);
-    event commentDonated(uint cId, address donator, uint value);
+    event questionAsked(address user, uint32 qId);
+    event answerAdded(address user, uint32 qId, uint32 cId);
+    event commentAdded(address user, uint32 qId, uint32 pcId, uint32 cId);
+    event questionAnswered(uint32 qId, uint32 cId, uint reward);
+    event commentDonated(uint32 cId, address donator, uint value);
+    event questionDonated(uint32 qId, address donator, uint value);
 
     struct User {
         bool isRegistered;
         string nickname;
-        uint[] ownQuestion;
-        uint[] ownComment;
+        uint32[] ownQuestion;
+        uint32[] ownComment;
         // More attributes?
     }
 
@@ -26,18 +30,21 @@ contract QnA {
         string title;
         // TODO: 改成ipfs/swarm hash
         string content;
-        uint[] comments;
+        uint32[] comments;
         bool isAnswered;
-        uint acceptedAnswer;
-        // TODO: 目前不能捐錢給提問者
+        uint32 acceptedAnswer;
+        uint received_val;
     }
 
     struct Comment {
         address payable owner;
+        // 紀錄所屬問題
+        uint32 questionId;
+        // 階層式留言，目前設定這個值是uint32最大值的話為回答，其他為留言，只有回答能被接受
+        uint32 parentId;
         // TODO: 改成ipfs/swarm hash
         string content;
         uint received_val;
-        // TODO: 目前留言=回答 要分開嗎? 階層式留言?(紀錄parentid)
     }
 
     modifier onlyOwner() {
@@ -52,7 +59,17 @@ contract QnA {
 
     // 建構子
     constructor() public payable {
+        donate_ratio = 10;
         owner = msg.sender;
+    }
+    
+    function changeDonateRatio(uint32 ratio) public onlyOwner {
+        donate_ratio = ratio;
+    }
+    
+    function uint32Max() private pure returns (uint32) {
+        uint32 i = 0;
+        return i - 1;
     }
     
     function register(string memory name) public {
@@ -71,7 +88,7 @@ contract QnA {
         newQ.title = title;
         newQ.content = content;
         // recored BEFORE append question(qId start from 0)
-        uint qId = questions.length;
+        uint32 qId = uint32(questions.length);
         questions.push(newQ);
         // Add connection between user and question
         members[msg.sender].ownQuestion.push(qId);
@@ -79,27 +96,49 @@ contract QnA {
         emit questionAsked(msg.sender, qId);
     }
     
-    function addComment(uint questionId, string memory content) public onlyMember {
-        require(questionId < questions.length, "Question not found!");
-        // new a Comment
+    function commentFactory(uint32 qId, string memory content, uint32 pcId) private returns (uint32) {
+        // 製造新的comment並且加進comment array
         Comment memory newCom;
         newCom.owner = msg.sender;
+        newCom.questionId = qId;
+        newCom.parentId = pcId;
         newCom.content = content;
         newCom.received_val = 0;
-        uint cId = comments.length;
+        uint32 cId = uint32(comments.length);
         comments.push(newCom);
+        return cId;
+    }
+    
+    function addAnswer(uint32 questionId, string memory content) public onlyMember {
+        require(questionId < questions.length, "Question not found!");
+        // new a Comment
+        uint32 cId = commentFactory(questionId, content, uint32Max());
         // Add connection between user and comment, comment and question
         questions[questionId].comments.push(cId);
         members[msg.sender].ownComment.push(cId);
         // Emit event
-        emit commentAdded(msg.sender, questionId, cId);
+        emit answerAdded(msg.sender, questionId, cId);
     }
     
-    function acceptAnswer(uint questionId, uint commentId) public onlyMember {
+    function addComment(uint32 questionId, uint32 parentCommentId, string memory content) public onlyMember {
+        require(questionId < questions.length, "Question not found!");
+        require(parentCommentId < comments.length, "Parent comment not found!");
+        require(parentCommentId != uint32Max(), "This is not an answer!");
+        // new a Comment
+        uint32 cId = commentFactory(questionId, content, parentCommentId);
+        // Add connection between user and comment, comment and question
+        questions[questionId].comments.push(cId);
+        members[msg.sender].ownComment.push(cId);
+        // Emit event
+        emit commentAdded(msg.sender, questionId, parentCommentId, cId);
+    }
+    
+    function acceptAnswer(uint32 questionId, uint32 commentId) public onlyMember {
         require(!questions[questionId].isAnswered, "This question already answered.");
         require(questions[questionId].owner == msg.sender, "You cannot accept answer!");
         require(questionId < questions.length, "Question not found!");
         require(commentId < comments.length, "Comment not found!");
+        require(comments[commentId].parentId == uint32Max(), "You cannot accept a comment!");
         // Give accepted answer question value
         uint reward = questions[questionId].value;
         comments[commentId].owner.transfer(reward);
@@ -110,21 +149,42 @@ contract QnA {
         emit questionAnswered(questionId, commentId, reward);
     }
     
-    function donateComment(uint commentId) public payable {
-        comments[commentId].owner.transfer(msg.value);
-        comments[commentId].received_val += msg.value;
+    function donateComment(uint32 commentId) public payable {
+        // 計算該給發問者跟回答者的斗內
+        uint toCommentMaker = (msg.value * (donate_ratio - 1)) / donate_ratio;
+        uint toQuestionMaker = msg.value - toCommentMaker; // 因為沒有浮點數所以用減的
+        
+        // 分別轉帳給發問者跟回答者
+        comments[commentId].owner.transfer(toCommentMaker);
+        uint32 qId = comments[commentId].questionId;
+        questions[qId].owner.transfer(toQuestionMaker);
+        
+        // 紀錄收到多少斗內
+        comments[commentId].received_val += toCommentMaker;
+        questions[qId].received_val += toQuestionMaker;
+        
         // Emit event
-        emit commentDonated(commentId, msg.sender, msg.value);
-        // TODO: 分給發問者
+        emit commentDonated(commentId, msg.sender, toCommentMaker);
+        emit questionDonated(qId, msg.sender, toQuestionMaker);
+    }
+    
+    function donateQuestion(uint32 questionId) public payable {
+        questions[questionId].owner.transfer(msg.value);
+        
+        // 紀錄收到多少斗內
+        questions[questionId].received_val += msg.value;
+        
+        // Emit event
+        emit questionDonated(questionId, msg.sender, msg.value);
     }
     
     // getters
-    function getAllQuestionIdByAddr(address addr) public view returns (uint[] memory) {
+    function getAllQuestionIdByAddr(address addr) public view returns (uint32[] memory) {
         require(members[addr].isRegistered, "User not found");
         return members[addr].ownQuestion;
     }
     
-    function getAllCommentIdByAddr(address addr) public view returns (uint[] memory) {
+    function getAllCommentIdByAddr(address addr) public view returns (uint32[] memory) {
         require(members[addr].isRegistered, "User not found");
         return members[addr].ownComment;
     }
