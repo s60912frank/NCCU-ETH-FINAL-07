35f743a53eb519e5a8e38e3b72018e34ee0d2127
travis Huang <travis965@gmail.com>
2019-01-10 12:55:02 +0800

    change method interface

--- docs/js/provider.js
+++ docs/js/provider.js
@@ -1,15 +1,14 @@
+let  web3MetamaskProvider,web3LocalhostProvider;
+const our_server_url = "http://localhost:8545";
 // login/logout section //
 const getProvider = async () => {
-  const our_server_url = "http://localhost:8545"
   try {
     if (typeof web3 !== 'undefined') {
       web3_Metamask = new Web3(web3.currentProvider);
       web3_localhost = new Web3(new Web3.providers.HttpProvider(our_server_url));
 
-      return {
-        "web3MetamaskProvider": web3_Metamask,
-        "web3LocalhostProvider": web3_localhost
-      }
+      web3MetamaskProvider  = web3_Metamask;
+      web3LocalhostProvider = web3_localhost;
 
     } else {
       console.log("no metamsk");
@@ -17,48 +16,58 @@ const getProvider = async () => {
       // no metamask
       web3_localhost = new Web3(new Web3.providers.HttpProvider(our_server_url));
 
-      return {
-        "web3MetamaskProvider": undefined,
-        "web3LocalhostProvider": web3_localhost
-      }
-
+      web3LocalhostProvider = web3_localhost;
     }
   } catch (error) {
     console.error(error);
   }
 }
 
+
 const login = async () => {
   try {
+    await getProvider();
+    let metaMaskUserObject,localAccounts;
 
-    const {
-      web3MetamaskProvider,
-      web3LocalhostProvider
-    } = await getProvider();
-    
-    if(web3MetamaskProvider){
+    try {
       const accounts = await web3MetamaskProvider.eth.getAccounts()
-      console.log(accounts);
       if(accounts.length < 1){
         throw new Error("Have metamask,No login");
       }
       const firstUser = accounts[0]
-
+  
       // set person img and name
       var icon = blockies.create({
         seed: firstUser,
       });
       $('#login-panel img').attr('src',icon.toDataURL());
       $('#login-panel p').text(firstUser.slice(2,8));
-      const metaMaskUserObject = {
+      metaMaskUserObject = {
           'account': firstUser,
           'img':icon.toDataURL()
       }
       localStorage.setItem('metaMaskUserObject', JSON.stringify(metaMaskUserObject));
       checkUser();
+
+      console.log("localStorage:metaMaskUserObject");
+      console.log(metaMaskUserObject);
+      
+    } catch (error) {
+      console.error(error);
+      alert("metamask problem");
     }
-    const localAccounts = await web3LocalhostProvider.eth.getAccounts()
-    console.log(localAccounts);
+
+    try {
+      localAccounts = await web3LocalhostProvider.eth.getAccounts()
+      console.log("local server:"+our_server_url);
+      
+      console.log(localAccounts);
+
+    } catch (error) {
+      console.error(error);
+      alert("server error");
+    }
+
     
     
   } catch (error) {
@@ -67,8 +76,27 @@ const login = async () => {
   }
 }
 
-const checkUser = ()=>{
+const logout = ()=>{
+  // remove user storage
+  localStorage.removeItem('metaMaskUserObject')
+  // remove web3 Provider
+  web3MetamaskProvider = undefined;
+  web3LocalhostProvider = undefined;
+  // remove user object
+  $('.metaMaskUser img').each(function(){
+    $(this).attr('src','');
+  })
+  $('.metaMaskUser p').each(function(){
+    $(this).text('name');
+  })
+  // toggle login/logout button
+  $( "#afterlogin" ).toggle("d-none");
+  $( "#beforelogin" ).toggle("d-none");
+}
+
+const checkUser = async ()=>{
     try {
+      await getProvider();
       const metaMaskUserObject = JSON.parse(localStorage.getItem('metaMaskUserObject'));
       console.log(metaMaskUserObject.account);
       console.log("user login");
@@ -80,24 +108,27 @@ const checkUser = ()=>{
       })
       $( "#afterlogin" ).toggle("d-none");
       $( "#beforelogin" ).toggle("d-none");
-      // $( "#beforelogin" ).toggle("d-none");
     } catch (error) {
+      // localstorage no metaMaskUserObject
+      logout()
       console.log("no user");
     }
 }
 
+const bindLoginButton = ()=>{
+  $('#beforelogin button').click(async function(){
+    // login button
+    await login();
+  });
 
-$('#beforelogin button').click(async function(){
-  // login button
-  await login();
-})
-$('#afterlogin button').click(function(){
-  // logout button
-  localStorage.removeItem('metaMaskUserObject')
-  $( "#afterlogin" ).toggle("d-none");
-  $( "#beforelogin" ).toggle("d-none");
-  console.log("remove");
-})
+  $('#afterlogin button').click(function(){
+    // logout button
+    logout()
+    console.log("log out");
+  })
+}
+
+
 
 
 // .... section //
