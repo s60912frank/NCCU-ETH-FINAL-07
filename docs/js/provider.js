01a6efa06a69f9f1e439f4b5e44997c8fd619c91
travis Huang <travis965@gmail.com>
2019-01-13 21:44:05 +0800

    remove localhost provider and localStorage account

--- docs/js/provider.js
+++ docs/js/provider.js
@@ -1,26 +1,14 @@
 //global variable
-let  web3MetamaskProvider,web3LocalhostProvider;
-const our_server_url = "http://localhost:8545";
-const our_server_accountNumber = 0;
+let  web3MetamaskProvider;
 // util section //
 const getProvider = async () => {
   try {
     if (typeof web3 !== 'undefined') {
       web3_Metamask = new Web3(web3.currentProvider);
-      web3_localhost = new Web3(new Web3.providers.HttpProvider(our_server_url));
-
       console.log("metamask connect");
       web3MetamaskProvider  = web3_Metamask;
-      console.log("local server connect");
-      web3LocalhostProvider = web3_localhost;
-
-    } else {
+    }else{
       console.log("no metamsk");
-      alert("no metamsk");
-      // no metamask
-      web3_localhost = new Web3(new Web3.providers.HttpProvider(our_server_url));
-
-      web3LocalhostProvider = web3_localhost;
     }
   } catch (error) {
     console.error(error);
@@ -52,7 +40,7 @@ const getFtrcContract = async()=>{
     .then(res => res.text());
   const abi = await fetch("./GlobalSetting/abi.json").then(res=>res.json());
 
-  let ftrc_forum = new web3LocalhostProvider.eth.Contract(abi);
+  let ftrc_forum = new web3MetamaskProvider.eth.Contract(abi);
   ftrc_forum.options.address = contractAddress;
   return ftrc_forum
 }
@@ -89,11 +77,11 @@ const getMappingComment = (comment)=>{
 const login = async () => {
   try {
     await getProvider();
-    let metaMaskUserObject,localAccounts;
-    const accountNumber = our_server_accountNumber;
+    let metaMaskUserObject;
+    const accountNumber = 0;
     try {
       // const accounts = await web3MetamaskProvider.eth.getAccounts()
-      const accounts = await web3LocalhostProvider.eth.getAccounts()
+      const accounts = await web3MetamaskProvider.eth.getAccounts()
       if(accounts.length < 1){
         throw new Error("Have metamask,No login");
       }
@@ -118,24 +106,6 @@ const login = async () => {
       console.error(error);
       alert("metamask problem");
     }
-
-    try {
-      const metaMaskUserObject = JSON.parse(localStorage.getItem('metaMaskUserObject'));
-      localAccounts = await web3LocalhostProvider.eth.getAccounts()
-
-      console.log("local server:"+our_server_url);
-      console.log("local server get user account[0]");
-      metaMaskUserObject.localAccount = localAccounts[accountNumber]
-      localStorage.setItem('metaMaskUserObject', JSON.stringify(metaMaskUserObject));
-      console.log(metaMaskUserObject);
-      
-
-    } catch (error) {
-      console.error(error);
-      alert("server error");
-    }
-
-    
     
   } catch (error) {
     console.error(error);
@@ -146,11 +116,6 @@ const login = async () => {
 const clear = ()=>{
   // remove user storage
   localStorage.removeItem('metaMaskUserObject')
-  // remove web3 Provider
-  console.log("remove web3MetamaskProvider");
-  web3MetamaskProvider = undefined;
-  // web3LocalhostProvider = undefined;
-  // remove user object
   $('.person-panel-box img').each(function(){
     $(this).attr('src','');
   })
