186ea7973e25977ec6b97e1816ef8c7b93f68023
Gmin <s60912frank@gmail.com>
2019-01-14 15:02:16 +0800

    contract: use revert to replace multiple require add checkExpire to deal with expired question (contract not tested yet).

--- contract/qna.sol
+++ contract/qna.sol
@@ -10,6 +10,8 @@ contract QnA {
     Comment[] public comments;
     QuestionType[] public questionstypes;
     uint32 donate_ratio;
+    uint32 constant uint32Max = 4294967295;
+    uint constant question_expire_after = 30; // 2592000 = 一個月
     
     /*
         Structs
@@ -18,7 +20,6 @@ contract QnA {
         uint received_val;
         uint32[] ownQuestion;
         uint32[] ownComment;
-        // More attributes?
     }
     
     struct QuestionType {
@@ -31,11 +32,10 @@ contract QnA {
         uint value;
         uint32 qTypeId;
         string title;
-        // TODO: 改成ipfs/swarm hash
         string content;
         uint32[] comments;
-        // bool isAnswered;
         // 如果acceptedAnswer == uint32最大值 表示這個問題還沒被回答
+        bool isAnswered;
         uint32 acceptedAnswer;
         uint received_val;
         uint time;
@@ -47,7 +47,6 @@ contract QnA {
         uint32 questionId;
         // 階層式留言，目前設定這個值是uint32最大值的話為回答，其他為留言，只有回答能被接受
         uint32 parentId;
-        // TODO: 改成ipfs/swarm hash
         string content;
         uint received_val;
         uint time;
@@ -67,7 +66,7 @@ contract QnA {
         Modifiers
     */
     modifier onlyOwner() {
-        require(owner == msg.sender, "you are not owner");
+        require(owner == msg.sender); // 要求是owner
         _;
     }
 
@@ -80,10 +79,6 @@ contract QnA {
     /*
         Private Functions
     */
-    
-    function uint32Max() private pure returns (uint32) {
-        return 4294967295;
-    }
     
     function transferAndRecordForComment(uint value, uint32 cId) private {
         address payable addr = comments[cId].owner;
@@ -127,9 +122,45 @@ contract QnA {
         questionstypes.push(qt);
     }
     
+    function checkExpire(uint32 questionId) public {
+        Question storage q = questions[questionId];
+        if(q.isAnswered // 已經被回答了
+            && now < q.time + question_expire_after) // 問題還沒過期
+            revert();
+        uint32 bestAnswerValId = uint32Max; // 預設最大值表示這個問題雖然完結，但沒有最佳解
+        if(q.comments.length == 0) {
+            // 沒人回QQ 退錢給發問者
+            q.owner.transfer(q.value);
+        }
+        else {
+            uint bestAnswerVal;
+            for(uint i = 1;i < q.comments.length;i++) {
+                uint32 cId = q.comments[i];
+                Comment memory c = comments[cId];
+                if(c.parentId == uint32Max 
+                    && c.received_val > bestAnswerVal) {
+                    bestAnswerVal = c.received_val;
+                    bestAnswerValId = cId;
+                }
+            }
+            // 把錢轉給最佳解
+            if(bestAnswerValId != uint32Max) {
+                transferAndRecordForComment(q.value, bestAnswerValId);
+            } else {
+                // 沒結果 怎辦
+                // 先退錢給發問者好ㄌ
+                q.owner.transfer(q.value);
+            }
+        }
+        // 紀錄這個問題已經回答了
+        q.isAnswered = true;
+        q.acceptedAnswer = bestAnswerValId;
+    }
+    
     function askQuestion(uint32 questionTypeId, string memory title, string memory content) public payable {
-        require(msg.value > 0, "You need to give reward!");
-        require(questionTypeId < questionstypes.length, "Type not found");
+        if(msg.value == 0 // 沒給獎勵
+            && questionTypeId < questionstypes.length) // 問題類型不存在
+            revert();
         // new a question
         Question memory newQ;
         newQ.owner = msg.sender;
@@ -137,7 +168,7 @@ contract QnA {
         newQ.title = title;
         newQ.content = content;
         newQ.qTypeId = questionTypeId;
-        newQ.acceptedAnswer = uint32Max();
+        // newQ.acceptedAnswer = uint32Max;
         newQ.time = now;
         // recored BEFORE append question(qId start from 0)
         uint32 qId = uint32(questions.length);
@@ -151,9 +182,11 @@ contract QnA {
     }
     
     function addAnswer(uint32 questionId, string memory content) public {
-        require(questionId < questions.length, "Question not found!");
+        if(questionId > questions.length // 問題不存在
+            && now > questions[questionId].time + question_expire_after) // 問題已經過期不能答了
+            revert();
         // new a Comment
-        uint32 cId = commentFactory(questionId, content, uint32Max());
+        uint32 cId = commentFactory(questionId, content, uint32Max);
         // Add connection between user and comment, comment and question
         questions[questionId].comments.push(cId);
         members[msg.sender].ownComment.push(cId);
@@ -162,9 +195,9 @@ contract QnA {
     }
     
     function addComment(uint32 questionId, uint32 parentCommentId, string memory content) public {
-        require(questionId < questions.length, "Question not found!");
-        require(parentCommentId < comments.length, "Parent comment not found!");
-        require(parentCommentId != uint32Max(), "This is not an answer!");
+        if(questionId > questions.length // 問題不存在
+            && parentCommentId > comments.length) // 回應的回答不存在
+            revert();
         // new a Comment
         uint32 cId = commentFactory(questionId, content, parentCommentId);
         // Add connection between user and comment, comment and question
@@ -177,23 +210,28 @@ contract QnA {
     function acceptAnswer(uint32 questionId, uint32 commentId) public {
         Question storage Q = questions[questionId];
         Comment memory A = comments[commentId];
-        require(Q.acceptedAnswer == uint32Max(), "This question already answered.");
-        require(Q.owner == msg.sender, "You cannot accept answer!");
-        require(questionId < questions.length, "Question not found!");
-        require(commentId < comments.length, "Comment not found!");
-        require(A.parentId == uint32Max(), "You cannot accept a comment!");
-        require(A.questionId == questionId, "Wrong questionId for comment!");
+        if(Q.isAnswered // 已經解答的不能再選
+            && Q.owner != msg.sender // 提問者才能選
+            && now > Q.time + question_expire_after // 問題已經過期
+            && questionId > questions.length // 問題id不存在
+            && commentId > comments.length // 回答id不存在
+            && A.parentId != uint32Max // 不能選回應
+            && A.questionId != questionId) // 回答id跟問題id不合
+            revert();
         // Give accepted answer question value
-        uint reward = questions[questionId].value;
-        transferAndRecordForComment(reward, commentId);
+        uint rewardToAnswer = Q.value * 80 / 100; // 80%給答題 20%返還給提問者
+        uint valueToQMaker = Q.value - rewardToAnswer;
+        Q.owner.transfer(valueToQMaker);
+        transferAndRecordForComment(rewardToAnswer, commentId);
         // Mark this question as answered
+        Q.isAnswered = true;
         Q.acceptedAnswer = commentId;
         // Emit event
-        emit questionAnswered(questionId, commentId, reward);
+        emit questionAnswered(questionId, commentId, rewardToAnswer);
     }
     
     function donateComment(uint32 commentId) public payable {
-        require(commentId < comments.length, "Comment not found!");
+        require(commentId < comments.length); // 找不到comment
         // 計算該給發問者跟回答者的斗內
         uint toCommentMaker = (msg.value * (donate_ratio - 1)) / donate_ratio;
         uint toQuestionMaker = msg.value - toCommentMaker; // 因為沒有浮點數所以用減的
@@ -210,7 +248,7 @@ contract QnA {
     }
     
     function donateQuestion(uint32 questionId) public payable {
-        require(questionId < questions.length, "Question not found!");
+        require(questionId < questions.length); // 找不到問題
         transferAndRecordForQuestion(msg.value, questionId);
         // Emit event
         emit questionDonated(questionId, msg.sender, msg.value);
@@ -228,7 +266,7 @@ contract QnA {
     }
     
     function getQuestionById(uint32 id) public view returns (address, uint, uint32, string memory, uint32, uint, uint, uint) {
-        require(id < questions.length, "Question not found!");
+        require(id < questions.length); // 找不到問題
         Question memory q = questions[id];
         // 依序回傳問題的: 發問者、獎勵、類別、標題、接受的回答id、收到多少斗內、發問時間、回應數量
         return (q.owner, q.value, q.qTypeId, q.title, q.acceptedAnswer, q.received_val, q.time, q.comments.length);
@@ -236,13 +274,13 @@ contract QnA {
     
     function getQuestionDetailById(uint32 id) public view returns (string memory, uint32[] memory) {
         // 這裡回傳問題的內容，以及回應的list
-        require(id < questions.length, "Question not found!");
+        require(id < questions.length);  // 找不到問題
         Question memory q = questions[id];
         return (q.content, q.comments);
     }
     
     function getCommentById(uint32 id) public view returns (address, uint32, uint32, string memory, uint, uint) {
-        require(id < comments.length, "Comment not found!");
+        require(id < comments.length); // 找不到回應
         Comment memory c = comments[id];
         // 依序回傳回答(回應)的: 回答者、所屬問題id、所屬回答id、內容、收到多少斗內、留言時間
         return (c.owner, c.questionId, c.parentId, c.content, c.received_val, c.time);
