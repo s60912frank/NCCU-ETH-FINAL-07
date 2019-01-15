91b1c4a419cabb9b8294b6e7cee3b6ed2db1589f
travis Huang <travis965@gmail.com>
2019-01-15 22:28:06 +0800

    postQuestion.html 1. change navbar 2. set value in input
    singleQuestion.html
    1. add donate like function
    2. show donate amount
    3. choose best answer
    4. outdated button add

--- docs/js/provider.js
+++ docs/js/provider.js
@@ -14,7 +14,7 @@ const getProvider = async () => {
     console.error(error);
   }
 }
-const checkUser = async ()=>{
+const checkUser = async (isToggle=true)=>{
   try {
     await getProvider();
     const metaMaskUserObject = JSON.parse(localStorage.getItem('metaMaskUserObject'));
@@ -26,12 +26,16 @@ const checkUser = async ()=>{
       $('.person-panel-box p').each(function(){
         $(this).text(metaMaskUserObject.account.slice(2,8));
       })
-      loginButtleToggle()
+      if(isToggle){
+        loginButtleToggle()
+      }
     }
+    return true;
   } catch (error) {
     // localstorage no metaMaskUserObject
     clear()
     console.log("no user");
+    return false;
   }
 }
 const getFtrcContract = async()=>{
@@ -39,25 +43,35 @@ const getFtrcContract = async()=>{
   await fetch("./GlobalSetting/address.txt")
     .then(res => res.text());
   const abi = await fetch("./GlobalSetting/abi.json").then(res=>res.json());
-
   let ftrc_forum = new web3MetamaskProvider.eth.Contract(abi);
   ftrc_forum.options.address = contractAddress;
   return ftrc_forum
 }
 const getMappingQuestion = (question)=>{
-  // 依序回傳問題的: 發問者、獎勵、類別、標題、接受的回答id、收到多少斗內、發問時間、回應數量
+  // 依序回傳問題的: 發問者、獎勵、類別、標題、是否已經被回答了、收到多少斗內、發問時間、回應數量
   const mappingQuestion = {
     "qsID": question.id,
     "asker": question[0],
     "reward": question[1],
     "qsType": question[2],
     "qsTitle": question[3],
-    "acceptAnswer": question[4],
+    "hasBestAnswer": question[4],
     "donate": question[5],
     "time": question[6],
-    "replyNumber": question[7]
+    "untilTime": question[7],
+    "replyNumber": question[8]
   }
   return mappingQuestion;
+}
+const getMappingQuestionDetail = (questionDetail)=>{
+  // 這裡回傳問題的內容，回應的list，是否已被解答，以及最佳解答id
+  const mappingQuestionDetail = {
+    "qsContent": questionDetail[0],
+    "commentList": questionDetail[1].map(ele=>parseInt(ele,10)),
+    "hasBestAnswer": questionDetail[2],
+    "bestAnswer": questionDetail[3],
+  }
+  return mappingQuestionDetail;
 }
 const getMappingComment = (comment)=>{
   // 依序回傳回答(回應)的: 回答者、所屬問題id、所屬回答id、內容、收到多少斗內、留言時間
@@ -72,6 +86,7 @@ const getMappingComment = (comment)=>{
   }
   return mappingComment;
 }
+
 
 // login/logout section //
 const login = async () => {
@@ -101,7 +116,8 @@ const login = async () => {
       checkUser();
       console.log("Metamask get user account[0]");
       console.log(metaMaskUserObject);
-      
+      location.reload();
+
     } catch (error) {
       console.error(error);
       alert("metamask problem");
@@ -140,6 +156,7 @@ const bindLoginButton = ()=>{
     clear();
     loginButtleToggle();
     console.log("log out");
+    location.reload();
   })
 }
 
@@ -268,11 +285,21 @@ const setProblemList = async() =>{
 // postQuestion section //
 const getAllQuestionTypes = async()=>{
   // getAllQuestionTypes
-  const ftrc_forum = await getFtrcContract();
-  const result = await ftrc_forum.methods.getAllQuestionTypes().call();
-  const resultArray = result.split(",");
-  console.log(resultArray);
-  return resultArray;
+  try {
+    const ftrc_forum = await getFtrcContract();
+    console.log(ftrc_forum);
+    const result = await ftrc_forum.methods.getAllQuestionTypes().call();
+    console.log(result);
+    if(result !== undefined){
+      const resultArray = result.split(",");
+      console.log(resultArray);
+      return resultArray;
+    }else{
+      throw new Error("type array unformated")
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const postQuestion = async(userAddress,qsType,qsTitle,qsContent,qsAmount)=>{
@@ -344,13 +371,18 @@ const getCommentById = async(_commentID)=>{
 
 const getQuestionAndAllAnswerFrom = async(_questionID)=>{
   let question = await getQuesionContent(_questionID);
-  const questionDetail = await getQuestionDetailById(_questionID);
   question.id = _questionID;
-  let mappingQuestion = getMappingQuestion(question)
-  mappingQuestion.qsContent = questionDetail[0];
+  const mappingQuestionShort = getMappingQuestion(question)
+  console.log(mappingQuestionShort);
+  
 
+  const questionDetail = await getQuestionDetailById(_questionID);
+  const mappingQuestionDetail = getMappingQuestionDetail(questionDetail);
+  console.log(mappingQuestionDetail);
+  
+  const mappingQuestion =  {"id":_questionID,...mappingQuestionShort,...mappingQuestionDetail}
 
-  let commentIDArray = questionDetail[1].slice().map(ele=>parseInt(ele,10));
+  let commentIDArray = mappingQuestion.commentList.slice().map(ele=>parseInt(ele,10));
   const commentObjectArray = await Promise.all(commentIDArray.map(async(commentID) => {
     let result = await getCommentById(commentID);
     result.id = commentID;
@@ -366,7 +398,7 @@ const getQuestionAndAllAnswerFrom = async(_questionID)=>{
 
 
 const setQuestionOnSingleQuestionHtml = (question)=>{
-
+  console.log(question); 
   const postDay = new Date(parseInt(question.time,10)*1000);
   const showDay = postDay.getFullYear()+"-"+(postDay.getMonth()+1)+"-"+postDay.getDate();
   const questionData = {
@@ -374,7 +406,9 @@ const setQuestionOnSingleQuestionHtml = (question)=>{
     qsAsker:question.asker.slice(2,8),
     qsDay: "張貼於:"+showDay,
     qsContent:question.qsContent,
-    qsAskerPicText:question.asker
+    qsAskerPicText:question.asker,
+    donateDisplay:"收到金額: "+ changeDonateToBetterShow(question.donate),
+    qsReward: "獎賞: "+ Web3.utils.fromWei(question.reward,"finney") +" Finney"
   }
   for (let [key, value] of Object.entries(questionData)) {
       $('#'+key).text(value);
@@ -386,46 +420,269 @@ const setQuestionOnSingleQuestionHtml = (question)=>{
         $('#qsAskerPic').attr('src',icon.toDataURL());
       }
   }
+  if(question.hasBestAnswer){
+    $('#badgeForBest').html('<span class="badge badge-success">已選出最佳解答</span>')
+  }else{
+    $('#badgeForBest').html('<span class="badge badge-light">尚未選出最佳解答</span>')
+  }
   
 } 
 
-const setAnswerOnSingleQuestionHtml = (commentArray)=>{
-  console.log(commentArray);
+const setAnswerQuestionOnSingleQuestionHtml = (question,userAddress)=>{
+  const {hasBestAnswer,asker,untilTime} = question
   
+  const nowTimestamp = +new Date();
+  const questionIsOutDate = nowTimestamp > untilTime*1000;
+  
+  if(asker === userAddress){
+    $('.sqPostAnswer').first().css("display","none");
+  }else{
+    if(questionIsOutDate){
+      $('.sqPostAnswer').first().css("display","none");
+    }else{
+      if(hasBestAnswer){
+        $('.sqPostAnswer').first().css("display","none");
+      }else{
+        $('#answerSubmit').click(async function () {
+          if(userAddress){
+            try {
+              await addAnswer(userAddress,questionID, $('#Answer').val());
+              alert("發布留言成功");
+              location.reload();
+            } catch (error) {
+              console.error(error);
+              alert("發布留言失敗");
+            }  
+          }else{
+            alert("發布留言失敗");
+          }
+        });
+      }
+    }
+  }
+}
+
+const setAnswerOnSingleQuestionHtml = (commentArray,question,userAddress)=>{
+  const {untilTime} = question
+  console.log(question);
+  
+
+  const buttonClass="donateCommentToAnswer"
+  const unitM = ['ether','finney','szabo','shannon','mwei','kwei','wei'];
+
+  const nowTimestamp = +new Date();
+  const questionIsOutDate = nowTimestamp > untilTime*1000;
+
+  //未選解答
+  //判斷是不是發問者，且沒有選解答
+  let showCheckBox = false;
+  if(question.asker === userAddress && !question.hasBestAnswer && !questionIsOutDate){
+    showCheckBox = true;
+  }else{
+    showCheckBox = false;
+  }
+
+  // add Comment List 
   $('#commentList').html('');
   commentArray.filter(ele=>{
+    //篩調非回答的
     return ele.belongCommentID === "4294967295"
   })
-  .forEach(ele=>{
+  .forEach((ele,index)=>{
     const iconUrl = blockies.create({seed: ele.answerUserID,}).toDataURL();
     const userName = ele.answerUserID.slice(2,8);
+    const content = ele.content;
+    const answerListID = ele.answerUserID+"-"+ele.cmID+"-"+ele.questionID;
+
+    const donateDisplay = changeDonateToBetterShow(ele.donate)
+    
+    let checkBox = "";
+    if(showCheckBox){
+      checkBox = `<input type="checkbox" class="checkBestAnswer" data-answer="${answerListID}"></input>`;
+    }
     const answerHtml = `
       <div class="media mb-4">
-        <img class="d-flex mr-3 rounded-circle" src="${iconUrl}">
-        <div class="media-body">
-          <h5 class="mt-0">${userName}</h5>
-          <p>${ele.content}</p>
+          ${checkBox}
+          <img class="d-flex mr-3 rounded-circle" src="${iconUrl}" alt="">
+          <div class="media-body">
+            <h5 class="mt-0">${userName}</h5>
+            <p>${content}</p>
+            
+            <p><span class="badge badge-primary">收到金額:</span>&nbsp&nbsp${donateDisplay}</p>
+          </div>
+          <div class="d-flex commentDonateList">
+            <div class="input-group mb-3">
+              <input type="number" min="1" required value="1" data-answer="${answerListID}">
+              <select data-answer="${answerListID}">
+                <option value="0">Ether</option>
+                <option value="1">Finney</option>
+                <option value="2">Szabo</option>
+                <option value="3">Shannon</option>
+                <option value="4">MWei</option>
+                <option value="5">KWei</option>
+                <option value="6">Wei</option>
+              </select>
+              <div class="input-group-prepend">
+                <button class="btn btn-outline-secondary ${buttonClass}" type="button" data-answer="${answerListID}"><img alt="Tweet" src="images/like.png"/></button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
     `
     $('#commentList').append(answerHtml)
   });
 
+  // set donate button function and will need user id
+  if(questionIsOutDate && !question.hasBestAnswer){
+    $('.commentDonateList .input-group').each(function(){
+      $(this).css('display','none');
+    })
+  }else{
+    $('.'+buttonClass).each(function(index){
+    
+      $(this).on("click",async function(){
+        try {
+          var dataAnswerKey = $(this).data('answer');
+          
+          const input = $('.input-group input[data-answer=' + dataAnswerKey + ']');
+          let inputVal = '1';
+          
+          if(input.val()===''){
+            alert('沒有輸入數字');
+            return;
+          }else if(input.val()<=0){
+            alert('數字最小為1');
+            return;
+          }else{
+            inputVal = input.val();
+          }
+  
+          const select = $('.input-group select[data-answer=' + dataAnswerKey + ']');
+          const selectID = select.children("option:selected").val();
+          console.log(inputVal);
+          console.log(unitM[selectID]);
+          const commentID = dataAnswerKey.split('-')[1];
+          console.log(commentID);
+  
+          if(userAddress){
+            await donateAnswer(userAddress,commentID,inputVal,unitM[selectID]);
+            alert("按讚成功");
+            location.reload();
+          }else{
+            alert("donate button will not work until logining and render");
+            alert("按讚失敗");
+            return 
+          }
+        } catch (error) {
+          console.log(error);
+          alert("按讚失敗");
+        }
+      })
+    })
+  }
+
+
+
+  if(showCheckBox){
+    $('input.checkBestAnswer').on('change', function() {
+        $('input.checkBestAnswer').not(this).prop('checked', false);  
+    });
+
+    // 添加選解答按鈕
+  
+    // !question.hasBestAnswer && question.asker === userAddress
+    $('#commentList').append('<button id="chooseTheBestAnswer" class="btn btn-outline-secondary" type="button" >選為最佳解答</button>')
+
+    $('#chooseTheBestAnswer').click(async function(){
+      try {
+        const dataString = $("input.checkBestAnswer:checked").first().data('answer')
+        const chooseCommentID = dataString.split('-')[1];
+        const questionID = dataString.split('-')[2];
+        // .data("answer")
+        if(checkUser(isToggle=false)){
+          const metaMaskUserObject = JSON.parse(localStorage.getItem('metaMaskUserObject'));
+          const userAddress = metaMaskUserObject.account;
+          const ftrc_forum = await getFtrcContract();
+          const newContractInstance = await ftrc_forum.methods.acceptAnswer(questionID,chooseCommentID).send({
+            from: userAddress,
+            gas: 3400000
+          });
+          console.log(newContractInstance);
+          alert("已選擇最佳答案");
+        }else{
+          // it shouldn't
+          console.log("no user");
+        }
+      } catch (error) {
+        console.log("chooseTheBestAnswerError");
+        console.log(error);
+        alert("無法選擇最佳答案")
+      }
+    })
+  }
+
+  //過期了
+  if(questionIsOutDate){
+    if(!question.hasBestAnswer){
+      if(commentArray.length > 0){
+        //幫忙發獎金XDDD
+        $('#commentList').append('<button id="checkExpired" class="btn btn-outline-warning" type="button" >幫忙選解答分錢囉</button>')
+      }else{
+        // alert("過期沒人回答")
+        //返回金額
+        $('#commentList').append('<button id="checkExpired" class="btn btn-outline-warning" type="button" >幫QQ，沒人回答，退問問題的人$$</button>')
+      }
+
+      $('#checkExpired').click(async function(){
+        try {
+          const ftrc_forum = await getFtrcContract();
+          const newContractInstance = await ftrc_forum.methods.checkExpire(question.id).send({
+            from: userAddress,
+            gas: 3400000
+          });
+          console.log(newContractInstance);
+          alert("觸發成功");
+          location.reload();
+        } catch (error) {
+          console.error(error);
+          alert("觸發失敗");
+        }
+      })
+
+
+    }
+  }
+
+
+  
 }
 
 const setQuestionAndAnswerList = async(questionID)=>{
   try {
     const {question,commentArray} = await getQuestionAndAllAnswerFrom(questionID)
+    let userAddress;
+    try {
+      const metaMaskUserObject = JSON.parse(localStorage.getItem('metaMaskUserObject'));
+      userAddress = metaMaskUserObject.account;
+      console.log(userAddress);
+    } catch (error) {
+      console.error(error);
+      console.log("no user id");
+    }
 
     setQuestionOnSingleQuestionHtml(question);
-    setAnswerOnSingleQuestionHtml(commentArray);
+
+    setAnswerQuestionOnSingleQuestionHtml(question,userAddress);
+
+    setAnswerOnSingleQuestionHtml(commentArray,question,userAddress);
     
   } catch (error) {
     console.error(error);
-    // window.location = '/singleQuestion.html?q=0';
+    window.location = '/singleQuestion.html?q=0';
   }
 }
 
+// answer section //
 const addAnswer= async(userAddress,questionId,content)=>{
 
   const ftrc_forum = await getFtrcContract();
@@ -441,6 +698,43 @@ const addAnswer= async(userAddress,questionId,content)=>{
   return newContractInstance;
 }
 
+const donateAnswer = async(userAddress,commentID,amount,amountUnit)=>{
+  console.log("donating");
+  const ftrc_forum = await getFtrcContract();
+  const newContractInstance = await ftrc_forum.methods.donateComment(commentID).send({
+    from: userAddress,
+    gas: 3400000,
+    value: Web3.utils.toWei(amount,amountUnit)
+  });
+  console.log(newContractInstance);
+  return newContractInstance;
+}
 
+const changeDonateToBetterShow = (donateString)=>{
+  try {
+    const unitM = ['ether','finney','szabo','shannon','mwei','kwei','wei'];
+    let fitString = "";
+    unitM.map(ele=>{
+      //轉換成特定單位
+      let donateUnitString = Web3.utils.fromWei(donateString,ele);
+      //只要整數部分
+      donateUnitString = donateUnitString.split(".")[0];
+      //大數轉換
+      let a = new Web3.utils.BN(donateUnitString,10)
+      let b = new Web3.utils.BN('1000',10)
+       //只取千位三數
+      let Remainder = a.mod(b).toString()
+      // console.log(Remainder);
+      // console.log(ele);
+      return `${Remainder} ${ele} `;
+    }).forEach(ele=>fitString+=ele);
+    return fitString;
+  } catch (error) {
+    console.log(error);
+    return
+  }
+
+  
+}
 
 // ... section //
